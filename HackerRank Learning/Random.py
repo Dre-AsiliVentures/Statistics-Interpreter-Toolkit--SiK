6704d228a1eeaaf99d3d8479e974a73373c8bc8a
ElectronicsDr <ghdmwendwa001@gmail.com>
2022-06-17 21:55:16 +0300

    Update Statistics Learning Documents
    Update Statistics Learning Documents

---
+++ HackerRank Learning/Random.py
@@ -0,0 +1,7 @@
+def random_count(count):
+    import random
+    rand_list=[]
+    for i in range (0,count):
+        rand[i]=rand_list.insert(random.rand.int(90,100))
+    return rand
+random_count(2)
