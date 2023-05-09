85edb1d4f2b68e4172a6b38dc6f3908038709b68
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-05-09 20:14:40 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
+#Test
 from Boolean_Statistics import normal_or_not
-
-
 prices=[30000,25000,23000,20000,18000,17500,17000,16000,15000,12000]
 pearson_kew=descriptive_stat.skewness(prices)
 print(pearson_kew)
